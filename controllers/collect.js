daca71618993cdfe2d06841122e41f6e34911164
Albert Dorfman <albertdorfman@gmail.com>
2023-01-25 18:21:31 -0500

    Created the 'collect' controller to handle webhooks

--- controllers/collect.js
+++ controllers/collect.js
@@ -1,2 +1,21 @@
 // Listening for requests from webhooks
 // Sends the requests to our PG and Mongo databases
+
+// /api/collect/
+
+Request = require("../models/request");
+const { addRequest, getRequestByRequestID } = require("../models/request");
+const collectRouter = require("express").Router();
+const bodyParser = require("body-parser");
+collectRouter.use(bodyParser.raw({ type: "*/*" }));
+
+collectRouter.post("/:uuid", async (req, res) => {
+  const head = req.headers;
+  const body = req.body.toString();
+  const { uuid } = req.params;
+  const method = req.method;
+  await addRequest(uuid, method, head, body);
+  res.sendStatus(200);
+});
+
+module.exports = collectRouter;
