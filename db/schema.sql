8d6e5db1e710a6985c5809c2ead54454d55ce6f7
Albert Dorfman <albertdorfman@gmail.com>
2023-01-25 14:51:22 -0500

    SQL Schema Changes

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,17 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-DROP TABLE IF EXISTS bins;
+DROP TABLE IF EXISTS bins CASCADE;
 CREATE TABLE bins (
   id serial PRIMARY KEY,
   uuid uuid NOT NULL DEFAULT uuid_generate_v4()
 );
 
-DROP TABLE IF EXISTS requests;
+
+DROP TABLE IF EXISTS requests CASCADE;
 CREATE TABLE requests (
-  id int PRIMARY KEY,
+  id serial PRIMARY KEY,
   http_method varchar(10),
   timestamp timestamp WITH TIME ZONE,
-  bin_id int REFERENCES bins(id) ON DELETE CASCADE
+  bin_id int NOT NULL REFERENCES bins(id) ON DELETE CASCADE
 );
+
