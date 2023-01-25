0bfbf721a477519b937b863de48ca695ae4b4bf9
Rachel West <rachwest@Rachels-Air.lan>
2023-01-25 17:53:57 -0600

    added bin routing logic

--- controllers/bins.js
+++ controllers/bins.js
@@ -1,1 +1,25 @@
-// Creating bins, displaying bins, displaying all requests related to bins.
+// Creating a new bin & displaying all bins
+
+const binModel = require('../models/bin.js');
+const express = require('express');
+const router =  require("express").Router();
+
+router.get("/api/bins", (request, response) => {
+  try {
+    const bins = binModel.allBins();
+    response.status(200).json(bins);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+router.post("/api/bins", (request, response) => {
+  try {
+    const newUUID = binModel.newBin().uuid;
+    response.status(200).send(newUUID);
+  } catch (error) {
+    response.status(500).send(error)
+  }
+});
+
+module.exports = router;
