b1910744a59b273b1551ead370e2e79ccd74a620
Luke Oguro <lukeoguro@gmail.com>
2023-01-25 00:08:17 -0500

    Implement initial collect API

--- controllers/collect.js
+++ controllers/collect.js
@@ -1,2 +1,17 @@
 // Listening for requests from webhooks
 // Sends the requests to our PG and Mongo databases
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.raw({ type: '*/*' }));
+
+router.all('/*', (req, res) => {
+  console.log(req.method) // method
+  console.log(req.url) // path (you'll need to split() for the uuid part of the path)
+  console.log(req.headers) // headers object (origin should be in here too)
+  console.log(req.body.toString()) // body
+  res.sendStatus(200);
+})
+
+module.exports = router;
