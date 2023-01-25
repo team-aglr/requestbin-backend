ad59ef3d46632ebaf084b6af3067583c102dac0e
Ginni Pinckert <gcpinckert@gmail.com>
2023-01-25 15:20:52 -0800

    Adds request and bin controllers to main app file

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ require("./db/pg.js");
 
 // Logging
 const morgan = require("morgan");
+const router = require("./controllers/collect");
 app.use(morgan("tiny"));
 
 // Collect API
 const collectRouter = require('./controllers/collect');
 app.use('/collect', collectRouter)
+
+// Requests and Bins APIs
+const requestController = require('./controllers/collect');
+router.use('/api/bins/:uuid/requests', requestController)
+
+const binController = require('./controllers/binController');
+router.use('/api/bins', binController)
 
 module.exports = app;
