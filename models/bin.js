971735c50b430745d0631880d50ee3d32343acbe
Rachel West <rachwest@Rachels-Air.lan>
2023-01-24 23:41:59 -0600

    added logic for reading and adding bins to psql db

--- models/bin.js
+++ models/bin.js
@@ -2,3 +2,30 @@
 
 // Create a bin using postgres
 // Fetch all bins using postgres
+
+const client = require('../db/pg.js');
+
+async function allBins() {
+  try {
+    const bins = await client.query("SELECT * FROM bins");
+    return bins.rows;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function newBin() {
+  try {
+    await client.query("INSERT INTO bins DEFAULT VALUES");
+    const newUUID = await client.query("SELECT uuid FROM bins ORDER BY id DESC LIMIT 1");
+    return newUUID.rows;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default {
+  newBin,
+  allBins
+};
+
