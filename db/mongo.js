49693a6c824aff2e05c0f59e42ce6880ee6b5ecb
Luke Oguro <lukeoguro@gmail.com>
2023-01-24 21:53:10 -0500

    Refactor mongo connection

--- db/mongo.js
+++ db/mongo.js
@@ -1,17 +1,13 @@
-// Export a function that connects to mongo
+// Connect to mongo
 const config = require("../utils/config");
 const mongoose = require("mongoose");
 
-function connectMongo() {
-  mongoose.set("strictQuery", false);
-  mongoose
-    .connect(config.MONGODB_URI)
-    .then(() => {
-      console.log("Connected to MongoDB");
-    })
-    .catch((error) => {
-      console.log("Error connecting to MongoDB:", error.message);
-    });
-}
-
-module.exports = connectMongo;
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error.message);
+  });
