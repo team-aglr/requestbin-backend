34ecc3293291c608ec102f2412a530c3069dc98c
Ginni Pinckert <gcpinckert@gmail.com>
2023-01-25 19:55:29 -0800

    Implement request router

--- controllers/requests.js
+++ controllers/requests.js
@@ -1,10 +1,28 @@
-const requestRouter = require('express').Router()
+const requestRouter = require('express').Router({ mergeParams: true })
 const request = require('../models/request')
 
 requestRouter.get('/', async (req, res) => {
-  const binUUID = req.baseUrl.split("/")[3] // we no longer have access to :uuid param
-  console.log(binUUID);
-  res.send(200)
+  const binUUID = req.params.uuid
+  try {
+    const reqList = await request.getRequestsByBinUUID(binUUID)
+    if (reqList.length > 0) {
+      res.json(reqList)
+    } else {
+      res.status(404).send()
+    }
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+requestRouter.get('/:id', async (req, res) => {
+  try {
+    const reqObj = await request.getRequestByRequestID(req.params.id)
+    res.json(reqObj)
+  } catch (err) {
+    console.error(err)
+    res.status(404).send()
+  }
 })
 
 module.exports = requestRouter
