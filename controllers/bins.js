f5ac42447325a3f0fb7eea77b00351f11450a75b
Rachel West <rachwest@Rachels-Air.lan>
2023-01-26 11:56:11 -0600

    changed get all bins query to include and sort by most recent request date with nulls put last and cleaned up all bin model function name occurrences

--- controllers/bins.js
+++ controllers/bins.js
@@ -1,12 +1,12 @@
 // Creating a new bin & displaying all bins
 
-const binModel = require('../models/bin.js');
+const Bin = require('../models/bin.js');
 const express = require('express');
 const router =  express.Router();
 
 router.get("/", async(request, response) => {
   try {
-    const bins = await binModel.allBins();
+    const bins = await Bin.all();
     response.status(200).json(bins);
   } catch (error) {
     response.status(500).send(error);
@@ -15,7 +15,7 @@ router.get("/", async(request, response) => {
 
 router.post("/", async(request, response) => {
   try {
-    const newUUID = await binModel.newBin();
+    const newUUID = await Bin.createNew();
     response.status(200).json(newUUID);
   } catch (error) {
     response.status(500).send(error)
