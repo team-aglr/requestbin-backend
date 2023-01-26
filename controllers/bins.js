c6713c94071275dbac03ab6ea0ba35ef365574eb
Rachel West <rachwest@Rachels-Air.lan>
2023-01-25 21:52:45 -0600

    added await to model function calls and removed path since paths have been added to app js file

--- controllers/bins.js
+++ controllers/bins.js
@@ -4,18 +4,18 @@ const binModel = require('../models/bin.js');
 const express = require('express');
 const router =  express.Router();
 
-router.get("/api/bins", (request, response) => {
+router.get("/", (request, response) => {
   try {
-    const bins = binModel.allBins();
+    const bins = await binModel.allBins();
     response.status(200).json(bins);
   } catch (error) {
     response.status(500).send(error);
   }
 });
 
-router.post("/api/bins", (request, response) => {
+router.post("/", (request, response) => {
   try {
-    const newUUID = binModel.newBin().uuid;
+    const newUUID = await binModel.newBin().uuid;
     response.status(200).send(newUUID);
   } catch (error) {
     response.status(500).send(error)
