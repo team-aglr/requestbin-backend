d7fb7c795a9511e8c2b18c0cbba421a27b1d5608
Ginni Pinckert <gcpinckert@gmail.com>
2023-01-25 16:32:48 -0800

    Parses out bin UUID from base url

--- controllers/requests.js
+++ controllers/requests.js
@@ -1,1 +1,10 @@
-// Displaying a single request pertaining to a bin
+const requestRouter = require('express').Router()
+const request = require('../models/request')
+
+requestRouter.get('/', async (req, res) => {
+  const binUUID = req.baseUrl.split("/")[3] // we no longer have access to :uuid param
+  console.log(binUUID);
+  res.send(200)
+})
+
+module.exports = requestRouter
